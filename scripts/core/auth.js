010fdfdde3efb47061b525dae4be8ffcb21ba741
Nguyễn Thành Trung <trungff07@gmail.com>
2025-06-23 12:01:12 +0700

    add auto logout if can't find the studentname

--- scripts/core/auth.js
+++ scripts/core/auth.js
@@ -1,7 +1,8 @@
 export function checkAuth() {
   const studentID = localStorage.getItem('studentID');
   const password = localStorage.getItem('password');
-  if (!studentID || !password) {
+  const studentName = localStorage.getItem('studentName');
+  if (!studentID || !password || !studentName) {
     window.location.href = 'login.html';
     return false;
   }
